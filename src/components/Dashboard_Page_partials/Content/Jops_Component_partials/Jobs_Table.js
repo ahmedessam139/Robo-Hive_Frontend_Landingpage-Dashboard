b35e8687dd0bdcc9f6d3ceb0e21282b9ffea4fbb
Ahmed_Essam <ahmedessam139ae@gmail.com>
2023-07-11 10:27:50 +0300

    Fix

--- src/components/Dashboard_Page_partials/Content/Jops_Component_partials/Jobs_Table.js
+++ src/components/Dashboard_Page_partials/Content/Jops_Component_partials/Jobs_Table.js
@@ -201,7 +201,7 @@ const Jobs_Table = ({ jobs, robots, packages ,getJops }) => {
 
   return (
     <div>
-      <div className="bg-white p-4 m-4 rounded-lg " style={{ height: '80vh' }}>
+      <div className="bg-white p-4 m-4 rounded-lg " style={{ minHeight: '80vh' }}>
         <div className="flex flex-col md:flex-row md:justify-between mb-2 items-center ">
           <p className="mb-2 text-3xl text-gray-500">Jobs</p>
           <div className="flex  justify-end p-2">
